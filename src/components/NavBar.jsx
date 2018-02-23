d0173927af540ad95c5ecfab243c2d25b267eb10
Guest User <Guest@Epicodus-1C.local>
2018-02-23 14:04:06 -0800

    implement linking in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Link } from 'react-router-dom';
 
 function NavBar() {
+  const link={
+
+  };
   return (
     <div>
-      <h1>NavBar works!</h1>
+      <style jsx>{`
+        background: aliceblue;
+      `}</style>
+    <Link style={link} to='/'>Home</Link>
+    <Link style={link} to='/location'>Location</Link>
+    <Link style={link} to='/inventory'>Inventory</Link>
     </div>
   );
 }
