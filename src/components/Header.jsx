a91446bb38f3bf543c1526b3bb0f1b659a91191d
Guest User <Guest@Epicodus-1C.local>
2018-02-23 16:12:07 -0800

    include a Google Slides link in n README.md

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,11 @@ import NavBar from "./NavBar";
 function Header() {
   return (
     <div>
+      <style jsx>{`
+        background: salmon;
+        width: 100%;
+        margin: 0;
+      `}</style>
       <h1>Header works!</h1>
       <NavBar />
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,12 +4,15 @@ import { Link } from 'react-router-dom';
 
 function NavBar() {
   const link={
-
+    margin: '20px',
+    color: 'red'
   };
   return (
     <div>
       <style jsx>{`
         background: aliceblue;
+        width: 100%;
+        margin: 0;
       `}</style>
     <Link style={link} to='/'>Home</Link>
     <Link style={link} to='/location'>Location</Link>
