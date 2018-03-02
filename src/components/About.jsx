7596b1006c319a6e34fe466a5f591b0e973f6ae1
Lena Ha <vermilionha@gmail.com>
2018-03-02 10:22:14 -0800

    issues with .jsx returning/tabbing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,22 +7,7 @@ function About(){
         background: salmon;
       `}</style>
       <h3>This is the About Section!</h3>
-      <h4>To-Do List:</h4>
-      <ul>
-        <li>Push a random Creature and stuff to the userCreatures array</li>
-        <li>Harvest Creature - remove them from the userCreatures array then add a random object to userItems array<ul>
-          <li>Alternatively, simply take an object from the Creature.</li>
-        </ul></li>
-        <li>Implement crafting functionality<ul>
-          <li>Remove items used in crafting from the userItems array</li>
-          <li>Add item into userItems array</li>
-        </ul></li>
-        <li>Drawings for header, logo, creatures, and items - perhaps other illustrations as well</li>
-        <li>Consider transitions/animations throughout the site</li>
-        <li>Cooldown for Explore/Harvesting</li>
-        <li>Weather API for Creature discovery chance</li>
-      </ul>
-    </div>
+    <p></p></div>
   );
 }
 
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -10,9 +10,21 @@ function News() {
       `}</style>
       <h1>Community News</h1>
       <h4>Witcherie is being worked on!</h4>
-      <p>Vestibulum iaculis nisl non ligula consectetur, at sodales magna placerat. Nam tempus convallis sapien, vitae auctor mauris varius et. In id tincidunt ipsum, in faucibus sapien. Praesent condimentum erat a metus pellentesque posuere. Ut egestas sagittis ex sit amet tristique. Maecenas quis neque turpis. Ut bibendum lobortis orci. Aliquam at mattis ex, dictum commodo nisl. Morbi vitae aliquam nunc. Proin non elit in diam viverra congue vel non orci. Aenean porttitor libero sed dolor scelerisque, eu mattis odio imperdiet. Mauris pretium imperdiet egestas. Donec nibh justo, auctor vitae nisl vitae, tincidunt sagittis leo. Nullam interdum nibh ut vestibulum tempus.</p>
-      <p>Quisque varius, risus at iaculis ultricies, lorem quam venenatis nisl, nec fringilla neque mi et ex. Sed at eleifend quam, eu imperdiet est. Etiam pharetra enim semper ipsum volutpat aliquam. Vestibulum elementum rhoncus nisl quis eleifend. Duis pharetra felis nec volutpat pharetra. Cras sed elit massa. Integer est dolor, cursus ac leo eu, consequat dapibus ipsum. Aenean vel orci arcu. Mauris eget erat ornare, hendrerit mauris sit amet, dignissim metus. Phasellus pharetra lorem urna, eu vehicula sem ultricies ut. Etiam fringilla nulla id dignissim lobortis. Etiam porttitor suscipit neque. Integer sagittis hendrerit nulla vel hendrerit. Vestibulum mattis leo consequat, fermentum leo a, accumsan nunc. Mauris sit amet dignissim neque.</p>
-      <p>Pellentesque vitae egestas magna. Donec id odio erat. Morbi sed urna nec augue vulputate tristique id at nisl. Aliquam molestie scelerisque enim, ac varius purus porta at. Ut scelerisque sagittis turpis, ac condimentum massa dictum ac. Pellentesque facilisis ultricies purus, id luctus lectus tincidunt ac. Praesent non efficitur nibh, condimentum volutpat odio. In hendrerit, nunc id luctus aliquet, sem dolor porta elit, in auctor magna dolor sed eros. Duis nisl purus, ultrices in venenatis id, euismod a leo. Maecenas faucibus cursus augue, vitae hendrerit risus consectetur euismod. In pellentesque posuere libero, non varius tortor efficitur vel.</p>
+        <h4>To-Do List:</h4>
+        <ul>
+          <li>Push a random Creature and stuff to the userCreatures array</li>
+          <li>Harvest Creature - remove them from the userCreatures array then add a random object to userItems array<ul>
+            <li>Alternatively, simply take an object from the Creature.</li>
+          </ul></li>
+          <li>Implement crafting functionality<ul>
+            <li>Remove items used in crafting from the userItems array</li>
+            <li>Add item into userItems array</li>
+          </ul></li>
+          <li>Drawings for header, logo, creatures, and items - perhaps other illustrations as well</li>
+          <li>Consider transitions/animations throughout the site</li>
+          <li>Cooldown for Explore/Harvesting</li>
+          <li>Weather API for Creature discovery chance</li>
+        </ul>
     </div>
   );
 }
